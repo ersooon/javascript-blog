92c82493fced25021d7d63594f2e01a5d2045011
eryk <kartofel312@gmail.com>
2025-10-03 22:22:47 +0200

    class list add

--- js/script.js
+++ js/script.js
@@ -5,15 +5,21 @@ function titleClickHandler(event){
 
     console.log('Link was clicked!');
 
-    /* remove class 'active' from all article links  */
+    /* [DONE] remove class 'active' from all article links  */
+
     const activeLinks = document.querySelectorAll('.titles a.active');
 
     for(let activeLink of activeLinks){
     activeLink.classList.remove('active');
     }
-    /* add class 'active' to the clicked link */
+    /* [IN PROGRESS] add class 'active' to the clicked link */
 
-    /* remove class 'active' from all articles */
+    console.log('clickedElement:', clickedElement);
+    const clickedElement = this;
+    clickedElement.classList.add('active');
+
+    /* [DONE] remove class 'active' from all articles */
+
     const activeArticles = document.querySelectorAll('article.active');
 
     for(let activeArticle of activeArticles){
