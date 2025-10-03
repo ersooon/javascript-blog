839caad6268c0a9ee8471d6931a75f157fb4a206
eryk <kartofel312@gmail.com>
2025-10-03 22:13:30 +0200

    js element class changing

--- js/script.js
+++ js/script.js
@@ -1,21 +1,29 @@
 'use strict';
 
 function titleClickHandler(event){
-  console.log(event);  
+    console.log(event);  
 
-  console.log('Link was clicked!');
+    console.log('Link was clicked!');
 
-  /* remove class 'active' from all article links  */
+    /* remove class 'active' from all article links  */
+    const activeLinks = document.querySelectorAll('.titles a.active');
 
-  /* add class 'active' to the clicked link */
+    for(let activeLink of activeLinks){
+    activeLink.classList.remove('active');
+    }
+    /* add class 'active' to the clicked link */
 
-  /* remove class 'active' from all articles */
+    /* remove class 'active' from all articles */
+    const activeArticles = document.querySelectorAll('article.active');
 
-  /* get 'href' attribute from the clicked link */
+    for(let activeArticle of activeArticles){
+    activeArticle.classList.remove('active');
+    }
+    /* get 'href' attribute from the clicked link */
 
-  /* find the correct article using the selector (value of 'href' attribute) */
+    /* find the correct article using the selector (value of 'href' attribute) */
 
-  /* add class 'active' to the correct article */
+    /* add class 'active' to the correct article */
 }
 
 const links = document.querySelectorAll('.titles a');
